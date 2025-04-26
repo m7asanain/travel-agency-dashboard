282c13e92003b31725ca6e7c48cc82b19aa47e62
Mustafa Hasanain <m7asanain@gmail.com>
2025-04-26 20:53:08 +0300

    mobile screen sidebar

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router";
 import { sidebarItems } from "~/constants";
 import { cn } from "~/lib/utils";
 
-const NavItems = () => {
+const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
   const user = {
     name: "Mustafa",
     email: "constact@mustafahasanain.com",
@@ -25,7 +25,7 @@ const NavItems = () => {
                   className={cn("group nav-item", {
                     "bg-primary-100 !text-white": isActive,
                   })}
-                  // onClick={handleClick}
+                  onClick={handleClick}
                 >
                   <img
                     src={icon}
