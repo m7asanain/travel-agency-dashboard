eddba30dde9a64488bd19e7ffd079e8d8bb8ad0a
Mustafa Hasanain <m7asanain@gmail.com>
2025-04-26 20:34:09 +0300

    large screen navbar

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,5 +1,21 @@
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { NavItems } from "components";
+import { Outlet } from "react-router";
+
 const AdminLayout = () => {
-  return <div className="admin-layout">AdminLayout</div>;
+  return (
+    <div className="admin-layout">
+      MobileSidebar
+      <aside className="w-full max-w-[270px] hidden lg:block">
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems />
+        </SidebarComponent>
+      </aside>
+      <aside className="children">
+        <Outlet />
+      </aside>
+    </div>
+  );
 };
 
 export default AdminLayout;
