15f3e4ca4f4090fa1f930fa0319e6ada4b11191e
Mustafa Hasanain <m7asanain@gmail.com>
2025-05-02 14:01:49 +0300

    users table static page

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,13 +1,99 @@
 import { Header } from "components";
+import {
+  ColumnsDirective,
+  ColumnDirective,
+  GridComponent,
+} from "@syncfusion/ej2-react-grids";
+import { users } from "~/constants";
+import { cn, formatDate } from "~/lib/utils";
 
 const AllUsers = () => {
   return (
-    <main className="dashboard wrapper">
+    <main className="all-users wrapper">
       <Header
-        title="Trips Pages"
-        description="Check out our current users in real time."
+        title="Manage Users"
+        description="Filter, sort, and access detaild user profiles."
       />
-      All users Page Contents
+
+      <GridComponent dataSource={users} gridLines="None">
+        <ColumnsDirective>
+          <ColumnDirective
+            field="name"
+            headerText="Name"
+            width="200"
+            textAlign="Left"
+            template={(props: UserData) => (
+              <div className="flex items-center gap-1.5 px-4">
+                <img
+                  src={props.imageUrl}
+                  alt="user"
+                  className="rounded-full size-8 aspect-square"
+                  referrerPolicy="no-referrer"
+                />
+                <span>{props.name}</span>
+              </div>
+            )}
+          />
+
+          {/* email */}
+          <ColumnDirective
+            field="email"
+            headerText="Email Address"
+            width="150"
+            textAlign="Left"
+          />
+
+          {/* joining */}
+          <ColumnDirective
+            field="dateJoined"
+            headerText="Date Joined"
+            width="120"
+            textAlign="Left"
+            template={({ joinedAt }: { joinedAt: string }) =>
+              formatDate(joinedAt)
+            }
+          />
+
+          {/* Trip Creation */}
+          <ColumnDirective
+            field="itineraryCreated"
+            headerText="Trip Created"
+            width="130"
+            textAlign="Left"
+          />
+
+          {/* Trip Creation */}
+          <ColumnDirective
+            field="status"
+            headerText="Type"
+            width="100"
+            textAlign="Left"
+            template={({ status }: UserData) => (
+              <article
+                className={cn(
+                  "status-column",
+                  status === "user" ? "bg-success-50" : "bg-light-300"
+                )}
+              >
+                <div
+                  className={cn(
+                    "size-1.5 rounded-full",
+                    status === "user" ? "bg-success-500" : "bg-gray-500"
+                  )}
+                />
+                <h3
+                  className={cn(
+                    "font-inter text-xs font-medium",
+                    status === "user" ? "text-success-700" : "text-gray-500"
+                  )}
+                >
+                  {status}
+                </h3>
+              </article>
+            )}
+          />
+        </ColumnsDirective>
+      </GridComponent>
     </main>
   );
 };
