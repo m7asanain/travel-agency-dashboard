ec8c704bd78092780239c054c35527df521deba2
Mustafa Hasanain <m7asanain@gmail.com>
2025-05-04 18:16:44 +0300

    trip form: country field (flag issue)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 import { Link, useLocation } from "react-router";
 import { cn } from "~/lib/utils";
 
@@ -35,6 +36,18 @@ const Header = ({ title, description, ctaText, ctaUrl }: Props) => {
           {description}
         </p>
       </article>
+
+      {ctaText && ctaUrl && (
+        <Link to={ctaUrl}>
+          <ButtonComponent
+            type="button"
+            className="button-class !h-11 !w-full md:w-[240px]"
+          >
+            <img src="/assets/icons/plus.svg" alt="plus" className="size-5" />
+            <span className="p-16-semibold text-white">{ctaText}</span>
+          </ButtonComponent>
+        </Link>
+      )}
     </header>
   );
 };
