df896590811c3c0e4f863838ed6123cdf4ff0148
Mustafa Hasanain <m7asanain@gmail.com>
2025-05-06 20:31:48 +0300

    trip details (header section)

---
+++ app/routes/admin/trip-detail.tsx
@@ -0,0 +1,66 @@
+import type { LoaderFunctionArgs } from "react-router";
+import { getTripById } from "~/appwrite/trips";
+import type { Route } from "./+types/trip-detail";
+import { parseTripData } from "~/lib/utils";
+import { Header, InfoPill } from "components";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const { tripId } = params;
+
+  if (!tripId) throw Error("Trip ID is required");
+
+  return await getTripById(tripId);
+};
+
+const TripDetail = ({ loaderData }: Route.ComponentProps) => {
+  const tripData = parseTripData(loaderData?.tripDetails);
+
+  const {
+    name,
+    duration,
+    itinerary,
+    travelStyle,
+    groupType,
+    budget,
+    interests,
+    estimatedPrice,
+    description,
+    bestTimeToVisit,
+    weatherInfo,
+    country,
+  } = tripData || {};
+
+  return (
+    <main className="travel-detail wrapper">
+      <Header
+        title="Trip Details"
+        description="View and edit AI-generated travel plans"
+      />
+
+      <section className="container wrapper-md">
+        <header>
+          <h1 className="p-40-semibold text-dark-100">{name}</h1>
+          <div className="flex items-center gap-5"></div>
+
+          <div className="flex items-center gap-5">
+            <InfoPill
+              text={`${duration} day plan`}
+              image="/assets/icons/calendar.svg"
+            />
+            <InfoPill
+              text={
+                itinerary
+                  ?.slice(0, 2)
+                  .map((item) => item.location)
+                  .join(", ") || ""
+              }
+              image="/assets/icons/location-mark.svg"
+            />
+          </div>
+        </header>
+      </section>
+    </main>
+  );
+};
+
+export default TripDetail;
